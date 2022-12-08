12a068eb12ebfc755ba8768434c8dca616cd4f81
semako123 <zosusemenoch@gmail.com>
2022-12-08 21:27:46 +0100

    complete whatGPT3 section create feature component Add feature components into the whatGPT3 section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./features.css";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div
+      className="gpt3__features section__margin section__padding"
+      id="features"
+    >
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          {" "}
+          The Future is Now and You Just Need to Realize It. Step into The
+          Future Today and Make If Happen.{" "}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin section__padding" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="All jointure ladyship insisted so humanity the friendly bachelor to as on by put impossible own apartments to. We so opinion friends me message as delight. Whole front of plate heard on ought. All jointure ladyship insisted so humanity the friendly bachelor to as on by put impossible own apartments to. We so opinion friends me message as delight. Whole front of plate heard on ought"
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination{" "}
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front of plate heard on ought"
+        />
+        <Feature
+          title="knowledgebase"
+          text="All jointure ladyship insisted so humanity the friendly bachelor to as on by put impossible own apartments to "
+        />
+        <Feature
+          title="Education"
+          text="All jointure ladyship insisted so humanity the friendly bachelor to as on by put impossible own apartments to "
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
