d1e2e6caaaafd2288a09c03a997733d2742691ff
semako123 <zosusemenoch@gmail.com>
2022-12-08 23:35:26 +0100

    Complete cta component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <div className="gpt3__cta section__margin section__padding">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to get Started</p>
+        <h3>Register today & start enjoying the endless opportunities</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
